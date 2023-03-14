2728b196081c7e7c40256e0f2f0b2378542ae924
Nurekov Erbol <64440211+fullmetalldev@users.noreply.github.com>
2023-03-14 13:33:37 +0600

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    document.querySelectorAll('.anim').forEach((el) => {
+        el.classList.add('svg')
+    })
+    document.querySelector('.anim2').classList.add('checkMark')
+})
